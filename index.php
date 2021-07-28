3a169455b88b14801f1a78c955d06784d8455d18
Niiimaru <taixizhitian@gmail.com>
2021-07-28 14:59:23 +0900

    素数問題

--- index.php
+++ index.php
@@ -12,3 +12,21 @@ for ($i = 1; $i <=100; $i++) {
         echo $i.'<br>';
     }
 }
+
+echo '<br><br>';
+
+for ($i = 1; $i <= 10000; $i++) {
+
+    $point = 0;
+  
+    for ($j = 1; $j <= $i; $j++) {
+      if ($i % $j === 0) {
+        $point += 1;
+      }
+    }
+  
+    if ($point == 2) {
+      echo $i .'<br>';
+    }
+  
+  }
